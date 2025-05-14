38c86ac1be7fca54baa21ecd66ab87d792cca8ac
dajeong01 <wpekwjd@naver.com>
2025-05-14 17:42:06 +0900

    feat:generic / lambda / interface

---
+++ src/main/java/lambda/아무거나가능한인터페이스.java
@@ -0,0 +1,7 @@
+package lambda;
+
+@FunctionalInterface
+public interface 아무거나가능한인터페이스 {
+    void 실행();
+//    void 실행2();  => @FunctionalInterface를 정의했기때문에 안돼
+}
--- src/main/java/oop/BasicUserService.java
+++ src/main/java/oop/BasicUserService.java
@@ -3,14 +3,7 @@ package oop;
 public class BasicUserService {
 
     private UserRepository userRepository = new BasicUserRepository();         // 업캐스팅이라 Basic이든 Custom이든 상관없음
-//    private BasicUserRepository userRepository2 = (BasicUserRepository) userRepository;   // 다운캐스팅
-//    private CustomUserRepository userRepository2 = (CustomUserRepository) userRepository;   // 다운캐스팅인데 이건 안됨
-    /*
-        why?
-        업캐스팅은 쉽게 말해서 선풍기를 가전제품으로 치겠다 -> 다운캐스팅도 같은 클래스로 내려줘야함 가전제품 -> 선풍기
-        그래서 업캐스팅을 쓸때는 5번줄에 Basic이든 Custom이든 상관없으나
-        다운 캐스팅을 할 때에는 업캐스팅했을때 형태 똑같이 맞춰줘야함
-     */
+
 
     public BasicUserService(UserRepository userRepository){
         this.userRepository = userRepository;
@@ -21,3 +14,12 @@ public class BasicUserService {
         userRepository.insert();
     }
 }
+
+//    private BasicUserRepository userRepository2 = (BasicUserRepository) userRepository;   // 다운캐스팅
+//    private CustomUserRepository userRepository2 = (CustomUserRepository) userRepository;   // 다운캐스팅인데 이건 안됨
+    /*
+        why?
+        업캐스팅은 쉽게 말해서 선풍기를 가전제품으로 치겠다 -> 다운캐스팅도 같은 클래스로 내려줘야함 가전제품 -> 선풍기
+        그래서 업캐스팅을 쓸때는 5번줄에 Basic이든 Custom이든 상관없으나
+        다운 캐스팅을 할 때에는 업캐스팅했을때 형태 똑같이 맞춰줘야함
+     */
